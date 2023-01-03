9ff7c778698b9dece554cc64e2ebac64b5b31b1b
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2023-01-03 16:21:30 +0100

    add create issue

--- routes/issues.js
+++ routes/issues.js
@@ -4,61 +4,54 @@ const Issue = require("../models/issue");
 const fs = require("fs");
 
 router.post("/", async (req, res) => {
-  var image = req.files.image;
-  // convert this image to base64cle
-  console.log(image.data);
+  try {
+    let parsedIssue = JSON.parse(req.body.data);
+    const newIssue = new Issue({
+      title: parsedIssue.title,
+      description: parsedIssue.description,
+      date: parsedIssue.date,
+      photo: req.files.image.data,
+      location: parsedIssue.location,
+      status: parsedIssue.status,
+      userId: parsedIssue.userId,
+      severity: parsedIssue.severity,
+      category: parsedIssue.category,
+      votes: {
+        userId: parsedIssue.votes.userId,
+        value: parsedIssue.votes.value,
+      },
+    });
 
-  // fs.writeFile("./O2.png", image.data, { encoding: "base64" }, function (err) {
-  //   console.log("File created");
-  // });
-  res.send("Ok");
-  // var imageAsBase64 = fs.readFileSync(image, "base64");
-  //   var imagename = Date.now();
-  //   try {
-  //     var image = req.files.image;
-  //     imagename =
-  //       imagename + "." + image.name.split(".")[image.name.split(".").length - 1];
-  //     image.mv("./images/" + imagename, function (error, result) {
-  //       if (error) throw error;
-  //     });
-  //     var parsedData = JSON.parse(req.body.data);
-  //     const news = new News({
-  //       title: parsedData.title,
-  //       content: parsedData.content,
-  //       photos: "./images/" + imagename,
-  //       date: parsedData.date,
-  //       field: parsedData.field,
-  //     });
-  //     const savedNews = await news.save();
-  //     res.send("News added successfully");
-  //   } catch (err) {
-  //     console.log("Error while adding news : " + err);
-  //     res.status().send("Error while adding news : " + err);
-  //   }
+    const savedIssue = await newIssue.save();
+    res.send(savedIssue);
+  } catch (error) {
+    console.log("Error while adding the issue : " + error);
+    res.status().send("Error while adding the issue : " + error);
+  }
 });
 
 router.get("/", async (req, res) => {});
 
 router.delete("/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const issue = await Issue.deleteOne({ _id: new ObjectId(id) });
-        res.status(200).json(issue);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+  try {
+    const id = req.params.id;
+    const issue = await Issue.deleteOne({ _id: new ObjectId(id) });
+    res.status(200).json(issue);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 router.put("/:id", async (req, res) => {});
 
 router.get("/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const issue = await Issue.findOne({ _id: new ObjectId(id) });
-        res.status(200).json(issue);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+  try {
+    const id = req.params.id;
+    const issue = await Issue.findOne({ _id: new ObjectId(id) });
+    res.status(200).json(issue);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
